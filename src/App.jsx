183ad647b474f25801bfb65e9bd8feac0c0d7301
Dicky Gustyanto <dickygustyanto12@gmail.com>
2026-04-18 11:36:16 +0700

    website_hbd_v11

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ const App = () => {
                   onClick={startExperience} 
                   className="px-14 py-5 bg-[#0284c7] text-white rounded-full font-black hover:scale-105 transition-all duration-300 shadow-2xl tracking-widest uppercase text-xs"
                 >
-                    Lanjut disini
+                    Lanjut disini, Nyalain Volume yaa ❤️
                 </button>
             </div>
           </motion.div>
